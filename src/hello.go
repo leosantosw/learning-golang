0644398fd9d9d29455afa4cd820606160d8e8204
leosantosw <leonardosantosx0077@gmail.com>
2025-02-12 10:57:51 -0300

    first commit

---
+++ src/hello.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Leo"
+	version := 1.0
+	
+	fmt.Println("Olá,", name)
+	fmt.Println("Este programa está na versão", version)
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Sair do programa")
+
+	var command int
+	fmt.Scan(&command)
+	fmt.Println("O comando escolhido foi", command)
+
+	switch command {
+		case 1:
+			fmt.Println("Monitorando...")
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 3:
+			fmt.Println("Saindo do programa...")
+		default:
+			fmt.Println("Comando inválido.")
+	}
+}
